5169fd6a20e0ff5fecac0dcf6671c2802eb431be
Kshitiz Jain <cauldronpumpkin@gmail.com>
2021-05-21 19:42:34 +0530

    Added waiting room functionality

--- Server.cpp
+++ Server.cpp
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <fstream>
 #include <cstdint>
+#include <semaphore.h>
 
 #define MAXREQ 36
 #define MAXQUEUE 5
@@ -20,10 +21,12 @@ unordered_map<pthread_t, pthread_cond_t> cond_map;
 pthread_mutex_t mLock = PTHREAD_MUTEX_INITIALIZER; 
 
 
+unordered_set<int> allRooms;
+unordered_set<int> availableRooms;
+unordered_map<int, string> roomNames;
+unordered_map<int, unordered_set<string>> roomMembers;
 
-set<int> allRooms;
-set<int> availableRooms;
-map<int, set<string>> roomMembers;
+unordered_map<int, vector<sem_t*>> roomSemaphores; 
 
 unordered_map<int, string> fd_user_map;
 unordered_map<string, int> user_fd_map;
@@ -66,34 +69,72 @@ void closeSocket(int &fd)
 }
 
 
-void joinRoom(int fd, string username) 
+void playingArea(int fd, sem_t* user_sem, int room_id)
 {
-  memset(::reqbuf, 0, MAXREQ);
-  ::n = read(fd, ::reqbuf, MAXREQ-1);
-  string msg = string(reqbuf);
-  int room_id;
-  if(msg == "?") 
+  sendMessage(fd, "You have entered the Playing Area !!\n");
+}
+
+
+void waitingArea(int fd, string username, int room_id)
+{
+  sendMessage(fd, "You have entered the Waiting Area !!\n");
+
+  sem_t temp_sem;
+  sem_init(&temp_sem, 0, 0);
+
+  roomSemaphores[room_id].push_back(&temp_sem);
+
+  if (roomMembers[room_id].size() < 4)
   {
-    room_id = *allRooms.begin();  
-  } 
-  else 
+    sem_wait(&temp_sem);
+  }
+  else
   {
-    room_id = stoi(msg);
-  } 
+    for (auto itr : roomSemaphores[room_id])
+    {
+      sem_post(itr);
+    }
+  }
+
+  playingArea(fd, &temp_sem, room_id);
+}
+
+
+void joinRoom(int fd, string username, int room_id) 
+{
   user_room_map[username] = room_id;
   roomMembers[room_id].insert(username);
+
   sendMessage(fd, "Welcome to Room: " + to_string(room_id));
+
+  waitingArea(fd, username, room_id);
+
   return;
 }
 
+
 void allAvailableRooms(int fd, string username, string instructionMsg)
 {
-  string availableRoomNumbers = "Room numbers available: ";
-  for(int it: availableRooms) {
-    availableRoomNumbers += (to_string(it) + ",");
+  string availableRoomNumbers = "Rooms available: \n";
+
+  int i = 0;
+  unordered_map<int, int> temp_room_id;
+  for (int it : availableRooms) 
+  {
+    temp_room_id[i] = it;
+    availableRoomNumbers += (to_string(i++) + "  --  " + roomNames[it] + '\n');
   }
+
   availableRoomNumbers.pop_back();
-  sendMessage(fd, availableRoomNumbers + "\n\n" + instructionMsg);
+  sendMessage(fd, availableRoomNumbers + '\n');
+
+  memset(::reqbuf, 0, MAXREQ);
+  ::n = read(fd, ::reqbuf, MAXREQ-1);
+  
+  string msg = string(reqbuf);
+
+  joinRoom(fd, username, temp_room_id[stoi(msg)]);
+
   return;
 }
 
@@ -101,13 +142,23 @@ void allAvailableRooms(int fd, string username, string instructionMsg)
 void createNewRoom(int fd, string username) 
 {
   int room_id = (rand() % MAX_ROOMS);
+
+  sendMessage(fd, "Room Name ?");
+  memset(::reqbuf, 0, MAXREQ);
+  ::n = read(fd, ::reqbuf, MAXREQ-1);
+  
+  string msg = string(reqbuf);
+
   allRooms.insert(room_id);
   availableRooms.insert(room_id);
+  roomNames[room_id] = msg;
   user_room_map[username] = room_id;
   roomMembers[room_id].insert(username);
+
+  waitingArea(fd, username, room_id);
+
   return;
 }
-
 
 
 void* serveClient(void* fd) 
@@ -146,7 +197,7 @@ void* serveClient(void* fd)
       {
         // available_users.erase(username);
         sendMessage(consockfd, "Type an available Room Id or ? for a random room.");
-        joinRoom(consockfd, username);
+        // joinRoom(consockfd, username);
       }
       
       else if (msg == "2")
@@ -161,7 +212,7 @@ void* serveClient(void* fd)
         sendMessage(consockfd, "Welcome to Room: " + to_string(user_room_map[username]));
       }
       
-      else if (msg == "q" || msg == "Q")
+      else if (msg == "q" || msg == "Q" || msg == "q\n")
       {
         cout << "clicked";
         closeSocket(consockfd);
